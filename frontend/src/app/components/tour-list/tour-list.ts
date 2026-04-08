b726c344908b11fc7fb895871ee796da4a724ea6
floerychristopher <realchristopherfloery@gmail.com>
2026-04-08 12:14:39 +0200

    feat: create reusable tour-list component with material design

---
+++ frontend/src/app/components/tour-list/tour-list.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Tour } from "../../models/tour.model";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-tour-list',
+  imports: [MatCardModule, MatButtonModule, RouterLink],
+  templateUrl: './tour-list.html',
+  styleUrl: './tour-list.css',
+})
+export class TourList {
+  @Input({ required: true }) tours: Tour[] = [];
+}
