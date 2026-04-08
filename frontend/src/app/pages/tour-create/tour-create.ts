1e143f86b632fb46d0dbbcb120e3a2942f47e541
floerychristopher <realchristopherfloery@gmail.com>
2026-04-08 13:37:14 +0200

    feat: implement tour creation with reactive forms validation

--- frontend/src/app/pages/tour-create/tour-create.ts
+++ frontend/src/app/pages/tour-create/tour-create.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TourService } from '../../services/tour';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tour-create',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './tour-create.html',
   styleUrl: './tour-create.css',
 })
-export class TourCreate {}
+export class TourCreate {
+
+  private tourService = inject(TourService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  // Form und Validation definieren:
+  tourForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    description: ['', Validators.required],
+    distance: ['', [Validators.required, Validators.min(0.1)]],
+    estimatedTime: ['', Validators.required]
+  });
+
+  onSubmit() {
+    if (this.tourForm.valid) {
+      this.tourService.addTour(this.tourForm.value as any);
+      this.router.navigate(['/home']);
+    } else {
+      console.log('Formular hat Fehler und wird nicht abgeschickt!');
+      this.tourForm.markAllAsTouched();
+    }
+  }
+}
--- frontend/src/app/services/tour.ts
+++ frontend/src/app/services/tour.ts
@@ -19,4 +19,21 @@ export class TourService {
   getTourById(id: number): Tour | undefined {
     return this.mockTours.find(tour => tour.id === id);
   }
+
+  // Create new tour
+  addTour(newTourData: Omit<Tour, 'id'>): void {
+
+    // Random id (zwischen 1 und 1000)
+    const newId = Math.floor(Math.random() * 100) + 1;
+
+    // Tour zusammenbauen
+    const newTour: Tour = {
+      ...newTourData,
+      id: newId
+    };
+
+    // Ins Array pushen
+    this.mockTours.push(newTour);
+    console.log('Tour hinzugefügt! Aktuelle Touren:', this.mockTours);
+  }
 }
