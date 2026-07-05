0a19bdc8ff5662221ede68a6ffac1867c48f8daf
if23b265 <if23b265@technikum-wien.at>
2026-07-05 18:44:33 +0200

    feat(backend): introduce global exception handler for REST APIs
    -Added a @RestControllerAdvice to intercept ResponseStatusExceptions. Ensures that CORS headers are preserved during HTTP 400 errors and
    error messages are returned as clean JSON to the frontend

---
+++ backend/src/main/java/at/technikum/tourplanner/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package at.technikum.tourplanner.backend.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+
+        // ex.getReason() holt Text aus OpenRouteService
+        errorResponse.put("message", ex.getReason());
+
+        return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(errorResponse);
+    }
+}
